404ec29cdd19bbe3d17abfa0720ac77b49fa9265
Ramesh <rameshw3offshore@gmail.com>
2018-11-15 16:53:34 +0530

    Initial commit

---
+++ app/_services/authentication.service.ts
@@ -0,0 +1,66 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: Http) { }
+
+    login(username: string, password: string) {
+
+
+        console.log("login");
+        let data = { username: username, password: password,grant_type:'password' };
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.post('/token', data, options)
+            .map((response: Response) => {
+                // login successful if there's a jwt token in the response
+                let user = response.json();
+                console.log(user);
+
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    //ssessionStorage.setItem('currentUser', JSON.stringify(user));
+                    sessionStorage.setItem('accessToken', user.token);
+
+
+                }
+
+                return user;
+            });
+
+
+
+
+
+        //let headers: Headers = new Headers();
+        //headers.append("Authorization", "Basic " + btoa(username + ":" + password));
+        //headers.append("Content-Type", "application/x-www-form-urlencoded");
+        //return this._http.post(url, data, { headers: headers })
+      
+        //return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
+
+        //return this.http.get('http://localhost:15383/api/user/', { headers: headers })
+        //    .map((response: Response) => {
+        //        // login successful if there's a jwt token in the response
+        //        let user = response.json();
+                
+        //        if (user && user.token) {
+        //            // store user details and jwt token in local storage to keep user logged in between page refreshes
+        //            localStorage.setItem('currentUser', JSON.stringify(user));
+                   
+                    
+        //        }
+
+        //        return user;
+        //    });
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+}
---
+++ app/_services/user.service.ts
@@ -0,0 +1,61 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+import { User } from '../_models/index';
+declare var $ : any;
+@Injectable()
+export class UserService {
+    constructor(private http: Http) { }
+
+    getAll() {
+        let username = $("#username").val();
+        let password = $("#password").val();
+        let headers: Headers = new Headers();
+        headers.append("Authorization", "Basic " + btoa(username + ":" + password));
+        headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+        return this.http.get('http://localhost:15383/api/user/', { headers:headers }).map((response: Response) => response.json());
+    }
+    
+    getById(id: number) {
+        //return this.http.get('http://localhost:15383/api/user/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    create(user: User) {
+
+        console.log(user);
+        //let userData = JSON.stringify(user);
+        //let headers = new Headers();
+        //headers.append("Content-Type", "application/json");
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+        //console.log(userData);
+        //return this.http.post('http://localhost:15383/api/user/createUsers', userData, this.jwt()).map((response: Response) => response.json());
+
+
+        return this.http.post('http://localhost:15383/api/user/createUsers', user, options).map((response: Response) => response.json());
+    }
+
+    update(user: User) {
+        //return this.http.put('/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json());
+    }
+
+    delete(id: number) {
+        //return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // private helper methods
+
+    //private jwt() {
+  
+    //    // create authorization header with jwt token
+    //    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    //    //let currentUser = JSON.parse(localStorage.getItem(this.create.name));
+
+    //    console.log("CurrentUser: "+currentUser);
+    //    if (currentUser && currentUser.token) {
+    //        let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
+    //        return new RequestOptions({ headers: headers });
+    //    }
+    //}
+}
